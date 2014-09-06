3faf6ba76d60dabeab224cad985fdb42b2b4abc1
GaryCarneiro <GaryCarneiro@github.com>
2014-09-06 23:56:27 +0530

    Adding a newline before we start logging

--- speedtest/speedtest.sh
+++ speedtest/speedtest.sh
@@ -15,8 +15,8 @@ if [ ! -d $LOG_DIR ] ; then
     mkdir $LOG_DIR
 fi
 
+echo >> $LOGFILE
 echo -n "$LOGTIME|"  >> $LOGFILE
 /usr/local/bin/speedtest --server 4064 | egrep  'Upload:|Download:' | tr '\n' '|'  >> $LOGFILE
-echo
 
 exit
